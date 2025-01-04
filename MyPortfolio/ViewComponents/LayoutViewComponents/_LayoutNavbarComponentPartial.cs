ee73e1be18094892efaeac2b1ad098a6d2db17d9
selindil <0selindil@gmail.com>
2025-01-04 15:44:43 +0300

    .

---
+++ MyPortfolio/ViewComponents/LayoutViewComponents/_LayoutNavbarComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortfolio.ViewComponents.LayoutViewComponents
+{
+	public class _LayoutNavbarComponentPartial : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+
+}
---
+++ MyPortfolio/ViewComponents/LayoutViewComponents/_LayoutSidebarComponentPartial.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortfolio.ViewComponents
+
+{
+	public class _LayoutSidebarComponentPartial : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+
+}
