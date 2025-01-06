b65c3205a7805e86f119e1e36720ac4b02b8e1df
selindil <0selindil@gmail.com>
2025-01-06 17:47:26 +0300

    .

--- MyPortfolio/Controllers/ExperienceController.cs
+++ MyPortfolio/Controllers/ExperienceController.cs
@@ -1,12 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
+using MyPortfolio.DAL.Entities;
 
 namespace MyPortfolio.Controllers
 {
     public class ExperienceController : Controller
     {
+		MyPortfolioContext context = new MyPortfolioContext();
         public IActionResult ExperienceList()
         {
-            return View();
+            var values = context.Experiences.ToList();
+            return View(values);
         }
-    }
+		[HttpGet]
+		public IActionResult CreateExperience()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult CreateExperience(Experience experience)
+		{
+			context.Experiences.Add(experience);
+			context.SaveChanges();
+			return RedirectToAction("ExperienceList");
+		}
+
+
+	}
 }
