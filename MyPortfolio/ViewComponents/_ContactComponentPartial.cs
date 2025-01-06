332717e9599a6afc8f8e0450b05667d4e91beddd
selindil <0selindil@gmail.com>
2025-01-07 01:13:52 +0300

    ..

--- MyPortfolio/ViewComponents/_ContactComponentPartial.cs
+++ MyPortfolio/ViewComponents/_ContactComponentPartial.cs
@@ -1,12 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
 
 namespace MyPortfolio.ViewComponents
 {
     public class _ContactComponentPartial : ViewComponent
     {
+        MyPortfolioContext portfolioContext = new MyPortfolioContext();
         public IViewComponentResult Invoke()
         {
-            return View();
+            var values = portfolioContext.Contacts.ToList();
+            return View(values);
         }
     }
 }
