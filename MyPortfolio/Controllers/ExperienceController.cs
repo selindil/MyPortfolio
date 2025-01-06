e2958d56419311605448ddc29af0d5ec745b1b00
selindil <0selindil@gmail.com>
2025-01-06 20:48:51 +0300

    .

--- MyPortfolio/Controllers/ExperienceController.cs
+++ MyPortfolio/Controllers/ExperienceController.cs
@@ -25,7 +25,27 @@ namespace MyPortfolio.Controllers
 			context.SaveChanges();
 			return RedirectToAction("ExperienceList");
 		}
+		public IActionResult DeleteExperience(int id)
+		{
+			var value = context.Experiences.Find(id);
+			context.Experiences.Remove(value);
+			context.SaveChanges();
+			return RedirectToAction("ExperienceList");
+		}
+		[HttpGet]
+		public IActionResult UpdateExperience(int id)
+		{
+			var value = context.Experiences.Find(id);
+			return View(value);
+		}
 
+		[HttpPost]
+		public IActionResult UpdateExperience(Experience experience)
+		{
+			context.Experiences.Update(experience);
+			context.SaveChanges();
+			return RedirectToAction("ExperienceList");
+		}
 
 	}
 }
